f9c510871921a4f040ae6633cbcb2e60d6362082
Ollieeryo <ollieeryo@gmail..com>
2023-12-11 10:09:25 +0800

    Remove build folder from version control

--- build/services/databaseService.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const prismaDisconnect = (prisma) => {
-    return prisma.$disconnect();
-};
-exports.default = prismaDisconnect;
